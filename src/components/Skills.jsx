b192ded89a4cfea8bdf75f715ddaf2a1e6ec8403
Mukul <mukulsinghbbsr@gmail.com>
2025-09-18 19:57:07 +0530

    Update resume link in Skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -34,7 +34,7 @@ const Skills = () => {
             </motion.div>
           ))}
         </div>
-  <a href="https://drive.google.com/file/d/1MF8RuflyMgoFp4qL5RNLXXmVsIHG19uD/view?usp=drive_link" target="_blank" rel="noopener noreferrer" className="inline-block px-6 py-3 bg-[#915EFF] text-white font-bold rounded-lg shadow hover:bg-[#7a3fdc] transition">Download Resume</a>
+  <a href="https://drive.google.com/file/d/1CB723P1pt873FPuLcBhNfmY3zbPPfq7u/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="inline-block px-6 py-3 bg-[#915EFF] text-white font-bold rounded-lg shadow hover:bg-[#7a3fdc] transition">Download Resume</a>
       </motion.div>
     </section>
   );
